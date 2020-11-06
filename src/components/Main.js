229a35373996ee93a70f85198e6d1bc2bddfb9c9
Tim Harshman <goteamtim+git@gmail.com>
2020-11-06 08:21:00 -0800

    Adding styles Add more logic to the game

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
+import '../style/Main.css'
 
-export default function Main(){
+export default function Main() {
     //const [value, onChangeText] = useState('Useless Placeholder');
-    const [deck, setDeck] = useState([]);
-    const [currentCard,setCurrentCard] = useState({'Jack':'0'});
+    const [ deck, setDeck ] = useState([]);
+    const [ currentCard, setCurrentCard ] = useState({ 'Jack': '0' });
+    const [ stats, setStats ] = useState({'spades':0,'diamonds':0,'hearts':0,'clubs':0})
+    const [ suit_1_count, setSuitOneCount ] = useState(0)
+    const [ suit_2_count, setSuitTwoCount ] = useState(0)
+    const [ suit_3_count, setSuitThreeCount ] = useState(0)
+    const [ suit_4_count, setSuitFourCount ] = useState(0)
 
-    const suits = [ 'spades', 'diamonds', 'hearts', 'clubs' ];
-    const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    const suits = ['spades', 'diamonds', 'hearts', 'clubs'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-    function _buildDeck()
-    {
+    function _buildDeck() {
         var deck, suit, value;
 
         deck = [];
@@ -19,36 +24,98 @@ export default function Main(){
             for (let k = 0; k < values.length; k++) {
                 value = values[k];
                 var card = new Object();
-                card[ suit ] = value;
-                deck.push( card );
+                card[suit] = value;
+                deck.push(card);
             }
         }
-        
+
         return deck;
     }
 
     useEffect(() => {
-        setDeck( _buildDeck() );
-    },[]);
+        setDeck(_buildDeck());
+    }, []);
 
-    function _onDeckClick()
-    {
-        var index, temp_deck;
+    function _onDeckClick() {
+        var index;
 
-        index = Math.floor(Math.random() * deck.length ) + 1;
+        // Add the current card to the stats
+
+        if (deck.length === 0)
+        {
+            // Stop the timer
+            return;
+        }else if ( deck.length === 1 )
+        {
+            setCurrentCard( deck[ 0 ] );
+            setDeck([])
+            return;
+        }else if ( deck.length != 52 )
+        {
+            _updateStats()
+        }
+// Probably a better way to shuffle or in addition to this.
+// https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+        index = Math.floor (Math.random() * deck.length );
+
         setCurrentCard( deck[ index ] );
 
-        temp_deck = deck.splice( index, 1 )
+        deck.splice( index, 1 )
 
-        setDeck( temp_deck );
+        setDeck( deck );
     }
 
-    return(
-        <div style={{display: "flex", position: "absolute", 'padding-top': 19 + "px"}}>
-            <div onClick={() => _onDeckClick()} >Deck</div>
-            <div >{Object.keys(currentCard)[0]}</div>
+    function _updateStats( currentCard )
+    {
+        var suit;
+        debugger;
+        suit = Object.keys( currentCard )[ 0 ];
+
+        try {
+            stats[ suit ] += Number( currentCard[ suit ] );
+        } catch (error) {
+            var card;
+
+            card = currentCard[ suit ]
+
+            switch (card) {
+                case 'A':
+                    card = 1
+                    break;
+                case 'J':
+                    card = 11
+                    break;
+                case 'Q':
+                    card = 12
+                    break;
+                case 'K':
+                    card = 13
+                    break;
             
-            <div ></div>
-      </div>
+                default:
+                    card = 0
+                    break;
+            }
+            stats[ suit ] += card;
+        }
+        
+        setStats( stats )
+    }
+
+    return (
+        <div id="game-container">
+            <div id="deck" onClick={() => _onDeckClick()} >Deck</div>
+            <div id="current-card">
+                <div>{Object.keys(currentCard)[0]}</div>
+                <div>{currentCard[ Object.keys(currentCard)[0] ]}</div>
+            </div>
+
+            <div id="stats">
+                <div class="stat-row" id="suit_1">Spades {stats.spades}</div>
+                <div class="stat-row" id="suit_2">Diamonds {stats.diamonds}</div>
+                <div class="stat-row" id="suit_3">Dearts {stats.hearts}</div>
+                <div class="stat-row" id="suit_4">Clubs {stats.clubs}</div>
+            </div>
+        </div>
     )
 }
